e452acfc79081dfaa874e4f0695ea89449312442
alex.tplante <alex123go@users.noreply.github.com>
2021-10-01 15:16:19 -0400

    Update the handling of the serial ports
    The software now update the list of connected serial ports and connect to the selected port.
    It also disable button that should not be pressed according if you're connected or not to the arduino.

--- QCL_gui/QCL_GUI.py
+++ QCL_gui/QCL_GUI.py
@@ -2,6 +2,8 @@ from PyQt5 import QtGui, Qt, QtCore, QtWidgets, uic
 
 import sys
 import time
+import serial.tools.list_ports
+
 
 import numpy as np
 
@@ -33,45 +35,178 @@ class QCL_GUI(QtWidgets.QWidget):
 	
 	def initUI(self):
 		self.pushButton_serial_connect.pressed.connect(self.startSerial)
+		self.pushButton_serial_connect.setEnabled(False)
 		self.pushButton_serial_disconnect.pressed.connect(self.closeSerial)
+		self.pushButton_serial_disconnect.setEnabled(False)
+
+		self.pushButton_Comb1_power.pressed.connect(self.powerComb1)
+		self.pushButton_Comb1_enable.pressed.connect(self.enableComb1)
 		
-		self.pushButton_Comb1_power.pressed.connect(lambda: self.powerComb(1))
-		self.pushButton_Comb1_enable.pressed.connect(lambda: self.enableComb(1))
+		self.pushButton_Comb2_power.pressed.connect(self.powerComb2)
+		self.pushButton_Comb2_enable.pressed.connect(self.enableComb2)
+
+		self.Comb1_setPoint.setText('NA mA') # Not yet implemented
+		self.Comb1_current.setText('NA mA')
+		self.Comb2_setPoint.setText('NA mA')
+		self.Comb2_current.setText('NA mA')
 		
-		self.pushButton_Comb2_power.pressed.connect(lambda: self.powerComb(2))
-		self.pushButton_Comb2_enable.pressed.connect(lambda: self.enableComb(2))
-		self.updateUI()
+		self.disableUI_QCL()
+		self.updateSerialList()
 		
 	def updateUI(self):
-		print('TODO')
+		print('TODO') # should implement a way to read digitalOutput in the arduino
+
+	def updateSerialList(self):
+		self.comboPorts.clear()
+		self.Port_list = self.getSerialList()
 		
+		if len(self.Port_list) == 0:
+			text = 'No serial port available'
+			self.comboPorts.addItem(text)
+			self.pushButton_serial_connect.setEnabled(False)
+		else:
+			for i in range(len(self.Port_list)):
+				self.comboPorts.addItem(self.Port_list[i])
+			self.pushButton_serial_connect.setEnabled(True)
+
+
+	def getSerialList(self):
+		comlist = serial.tools.list_ports.comports()
+		connected = []
+		for element in comlist:
+		    connected.append(element.device)
+		return connected
+
+	def disableUI_QCL(self):
+		self.pushButton_Comb1_power.setEnabled(False)
+		self.pushButton_Comb1_enable.setEnabled(False)
+		self.pushButton_Comb2_power.setEnabled(False)
+		self.pushButton_Comb2_enable.setEnabled(False)
+
+		self.pushButton_serial_connect.setEnabled(True)
+		self.pushButton_serial_disconnect.setEnabled(False)
+		self.comboPorts.setEnabled(True)
+
+		self.pushButton_Comb1_power.setText('Turn power ON')
+		self.pushButton_Comb1_enable.setText('Enable output')
+		self.pushButton_Comb2_power.setText('Turn power ON')
+		self.pushButton_Comb2_enable.setText('Enable output')
+		self.pushButton_Comb1_power.setStyleSheet('')
+		self.pushButton_Comb1_enable.setStyleSheet('')
+		self.pushButton_Comb2_power.setStyleSheet('')
+		self.pushButton_Comb2_enable.setStyleSheet('')
+
+	def enableUI_QCL(self):
+		self.pushButton_Comb1_power.setEnabled(True)
+		self.pushButton_Comb1_enable.setEnabled(True)
+		self.pushButton_Comb2_power.setEnabled(True)
+		self.pushButton_Comb2_enable.setEnabled(True)
+
+		self.pushButton_serial_connect.setEnabled(False)
+		self.pushButton_serial_disconnect.setEnabled(True)
+		self.comboPorts.setEnabled(False)
+
+		# since we know that the output reset to 0 when we reconnect (maybe use updateUI in the future)
+		self.pushButton_Comb1_power.setText('Turn power ON')
+		self.pushButton_Comb1_enable.setText('Enable output')
+		self.pushButton_Comb2_power.setText('Turn power ON')
+		self.pushButton_Comb2_enable.setText('Enable output')
+		self.pushButton_Comb1_power.setStyleSheet('background-color: red')
+		self.pushButton_Comb1_enable.setStyleSheet('background-color: red')
+		self.pushButton_Comb2_power.setStyleSheet('background-color: red')
+		self.pushButton_Comb2_enable.setStyleSheet('background-color: red')
 		
 	def startSerial(self):
 		print('Opening serial port')
-		self.qcl.connect(port = 'COM9')
-		print('started')
-		# arduino reset at serial connection (but why?)
+		index = self.comboPorts.currentIndex()
+		port = self.Port_list[index]
+		print(port)
+		self.qcl.connect(port = port)
+		# arduino reset GPIO at serial connection
 		self.comb_power = [0,0]
 		self.comb_enable = [0,0]
+		self.enableUI_QCL()
 		
 	def closeSerial(self):
 		print('closing serial port')
 		self.qcl.disconnect()
+		self.disableUI_QCL()
+		self.updateSerialList()
 		
-	def powerComb(self,comb):
+	def powerComb1(self):
+		comb = 1
+		
 		actual_state = self.comb_power[comb-1]
 		new_state = int(not(actual_state))
 		self.qcl.powerComb(comb, new_state)
 		self.comb_power[comb-1] = new_state
-		self.updateUI()
+		#self.updateUI()
+
+		if new_state == 0:
+			#QCL is not off, turn button red and change text to 'Turn power ON'
+			self.pushButton_Comb1_power.setText('Turn power ON')
+			self.pushButton_Comb1_power.setStyleSheet('background-color: red')
+		else:
+			#QCL is not on, turn button green and change text to 'Turn power OFF'
+			self.pushButton_Comb1_power.setText('Turn power OFF')
+			self.pushButton_Comb1_power.setStyleSheet('background-color: green')
+
 		
-	def enableComb(self,comb):
+		
+	def enableComb1(self):
+		comb = 1
+
 		actual_state = self.comb_enable[comb-1]
 		new_state = int(not(actual_state))
 		self.qcl.enableComb(comb, int(not(actual_state)))
 		self.comb_enable[comb-1] = new_state
-		self.updateUI()
+		#self.updateUI()
+
+		if new_state == 0:
+			#QCL is not off, turn button red and change text to 'Enable output'
+			self.pushButton_Comb1_enable.setText('Enable output')
+			self.pushButton_Comb1_enable.setStyleSheet('background-color: red')
+		else:
+			#QCL is not on, turn button green and change text to 'Disable output'
+			self.pushButton_Comb1_enable.setText('Disable output')
+			self.pushButton_Comb1_enable.setStyleSheet('background-color: green')
 	
+	def powerComb2(self):
+		comb = 2
+		
+		actual_state = self.comb_power[comb-1]
+		new_state = int(not(actual_state))
+		self.qcl.powerComb(comb, new_state)
+		self.comb_power[comb-1] = new_state
+		#self.updateUI()
+
+		if new_state == 0:
+			#QCL is not off, turn button red and change text to 'Turn power ON'
+			self.pushButton_Comb2_power.setText('Turn power ON')
+			self.pushButton_Comb2_power.setStyleSheet('background-color: red')
+		else:
+			#QCL is not on, turn button green and change text to 'Turn power OFF'
+			self.pushButton_Comb2_power.setText('Turn power OFF')
+			self.pushButton_Comb2_power.setStyleSheet('background-color: green')
+
+
+	def enableComb2(self):
+		comb = 2
+
+		actual_state = self.comb_enable[comb-1]
+		new_state = int(not(actual_state))
+		self.qcl.enableComb(comb, int(not(actual_state)))
+		self.comb_enable[comb-1] = new_state
+		#self.updateUI()
+
+		if new_state == 0:
+			#QCL is not off, turn button red and change text to 'Enable output'
+			self.pushButton_Comb2_enable.setText('Enable output')
+			self.pushButton_Comb2_enable.setStyleSheet('background-color: red')
+		else:
+			#QCL is not on, turn button green and change text to 'Disable output'
+			self.pushButton_Comb2_enable.setText('Disable output')
+			self.pushButton_Comb2_enable.setStyleSheet('background-color: green')
 
 	
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -5,6 +5,12 @@ Control a QCL driver (QCL500 OEM or QCL 1500 OEM) via the GPIO pins of an arduin
 The python software runs a PyQt5 graphical user interface (GUI) and send serial commands according the the button pressed.
 Note : Due to the reset configuration of the Arduino, all outputs are reseted when a new serial connection is made. Therefore, the QCL's outputs are all turned off when you reconnect to the arduino.
 
+To run the python software, either
+```
+python QCL_GUI.py
+```
+or run the .exe file.
+
 ## Arduino
 The arduino software allows controlling a QCL driver via a python user interface.
 It controls 4 output pins according to the serial messages received.
